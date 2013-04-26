a3d7520e915b08271facb030c0699fbb4e74ad03
lorkalon <lorkalon@mail.ru>
2013-04-26 23:27:25 +0300

    Added Auto View and Details View

---
+++ CarForRent/Scripts/AutoBitch.js
@@ -0,0 +1,8 @@
+﻿$(function() {
+    $(".Car").on('click', function(event, ui) {
+        var $this = $(this);
+        var carId = $this.find(".CarId").val();
+        document.location ="/Auto/Details/" + carId;
+    });
+
+});
